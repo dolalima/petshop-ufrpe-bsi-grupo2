905c1445d3ee0ce989877869b8918165986faf72
dolalima <dolalima@gmail.com>
2011-03-18 19:16:28 +0000

    Adicionada a classe vazia BaseDeDados

---
+++ PetShop/src/petshop/BaseDeDados.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package petshop;
+
+/**
+ *
+ * @author Dola
+ */
+public class BaseDeDados {
+
+}
