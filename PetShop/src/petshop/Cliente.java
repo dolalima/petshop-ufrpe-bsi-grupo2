f51176190f616fd23b13162381eb1ffc6c5c8212
dolalima <dolalima@gmail.com>
2011-03-21 02:59:34 +0000

    editando a classe.

--- PetShop/src/petshop/Cliente.java
+++ PetShop/src/petshop/Cliente.java
@@ -10,5 +10,31 @@ package petshop;
  * @author Dola
  */
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private String rg;
+
+    public Cliente(){
+
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public void setRg(String rg){
+        this.rg = rg;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public String getCpf(){
+        return this.cpf;
+    }
+    public String getRg(){
+        return this.rg;
+    }
+
 
 }
