103b2adbbbec73a4a55c1ceec379f13170fef215
dolalima <dolalima@gmail.com>
2011-04-02 16:57:03 +0000

    Adicionada pasta para a interface

---
+++ PetShop/src/petshop/classes/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package petshop.classes;
+
+/**
+ *
+ * @author Dola
+ */
+public class Cliente extends BaseDeDados{
+    /*Variaveis que vão conter as imformações do Cliente*/
+   
+
+
+}
---
+++ PetShop/src/petshop/classes/Usuario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package petshop.classes;
+
+/**
+ *
+ * @author dola
+ */
+public class Usuario {
+
+}
