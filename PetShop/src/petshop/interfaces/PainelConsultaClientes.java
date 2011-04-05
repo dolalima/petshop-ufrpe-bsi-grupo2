64e9df878effe02ffcbd36f55508f397494ba4cf
arthur.hvt <arthur.hvt@1149b242-d79d-a3aa-b50b-b1633aa3294a>
2011-04-05 12:43:40 +0000

    Os paineis foram substituidos por janelas. A Janela de cadastro de clientes foi toda validada.

---
+++ PetShop/src/petshop/interfaces/PainelConsultaClientes.java
@@ -0,0 +1,48 @@
+package petshop.interfaces;
+
+
+public class PainelConsultaClientes extends PainelConsulta {
+
+    public PainelConsultaClientes(){
+        super();
+
+        comboPesquisa.setModel(new javax.swing.DefaultComboBoxModel(
+                new String[] { "Código", "Nome", "CPF", "RG" }));
+
+        tabela.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null}
+            },
+            new String [] {
+                "Código", "Nome", "CPF", "RG"
+            }
+        ));
+
+        botaoNovo.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                botaoNovoEvento(evt);
+            }
+        });
+    }
+
+
+    private void botaoNovoEvento(java.awt.event.MouseEvent evt) {
+        JanelaCadastroClientes cadastro = new JanelaCadastroClientes();
+
+        cadastro.setModalityType(java.awt.Dialog.DEFAULT_MODALITY_TYPE);
+        cadastro.setModal(true);
+        cadastro.setVisible(true);
+    }
+}
---
+++ PetShop/src/petshop/interfaces/PainelConsultaProdutos.java
@@ -0,0 +1,95 @@
+package petshop.interfaces;
+
+import java.awt.Dimension;
+import javax.swing.JTextField;
+
+
+public class PainelConsultaProdutos extends PainelConsulta {
+    
+    private JTextField campoAcimaDe;
+    private JTextField campoAbaixoDe;
+    private java.awt.GridBagConstraints gridBagConstraints;
+
+    public PainelConsultaProdutos(){
+        super();
+        
+        campoAcimaDe = new JTextField("Acima de");
+        campoAbaixoDe = new JTextField("Abaixo de");
+
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.ipadx = 117;
+        gridBagConstraints.insets = new java.awt.Insets(10, 10, 10, 10);
+        add(campoAcimaDe, gridBagConstraints);
+        gridBagConstraints.insets = new java.awt.Insets(10, 0, 10, 10);
+        gridBagConstraints.gridx = 3;
+        add(campoAbaixoDe, gridBagConstraints);
+
+
+        comboPesquisa.setModel(new javax.swing.DefaultComboBoxModel(
+                new String[] { "Código", "Nome", "Preço de Custo", "Preço de Venda" }));
+
+        comboPesquisa.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                comboPesquisaEvento(evt);
+            }
+        });
+
+        comboPesquisa.setMaximumSize(new Dimension(66,24));
+        comboPesquisa.setMinimumSize(new Dimension(66,24));
+
+        tabela.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null},
+                {null, null, null, null}, {null, null, null, null}, {null, null, null, null}
+            },
+            new String [] {
+                "Código", "Nome", "CPF", "RG"
+            }
+        ));
+
+        botaoNovo.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                botaoNovoEvento(evt);
+            }
+        });
+    }
+
+
+    private void botaoNovoEvento(java.awt.event.MouseEvent evt) {
+        JanelaCadastroProdutos cadastro = new JanelaCadastroProdutos();
+
+        cadastro.setModalityType(java.awt.Dialog.DEFAULT_MODALITY_TYPE);
+        cadastro.setModal(true);
+        cadastro.setVisible(true);
+    }
+
+
+   private void comboPesquisaEvento(java.awt.event.ItemEvent evt){
+       
+       if(comboPesquisa.getSelectedIndex() >= 2){
+           campoPesquisa.setVisible(false);
+           campoAcimaDe.setVisible(true);
+           campoAbaixoDe.setVisible(true);
+       } else {
+           campoPesquisa.setVisible(true);
+           campoAcimaDe.setVisible(false);
+           campoAbaixoDe.setVisible(false);
+       }
+   }
+}
