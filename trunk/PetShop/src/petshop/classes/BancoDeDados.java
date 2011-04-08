6f4a2e2d91250acb18a2aef713e2336fb2b435d3
dolalima@gmail.com <dolalima@gmail.com@1149b242-d79d-a3aa-b50b-b1633aa3294a>
2011-04-08 00:17:48 +0000

    adicionado mais alguns metodos

--- trunk/PetShop/src/petshop/classes/BancoDeDados.java
+++ trunk/PetShop/src/petshop/classes/BancoDeDados.java
@@ -8,6 +8,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
+
+/**
+ * Classe de selação do sistema com banco de dados, as atividades de cadastrar, pesquisar,
+ * alterar e excluir cliente, protudos, serviços e animais são todas feitas atravez dessa
+ * classe.
+ * @author dola
+ */
 public abstract class BancoDeDados {
     
     /**Endereço do Banco de Dados**/
@@ -21,7 +28,7 @@ public abstract class BancoDeDados {
     /**Variavel de recebe os metadados das pesquisas no banco de dados. **/
     private ResultSetMetaData resultsetmetadata = null;
 
-    /**Metodo para conectar do sistema ao banco de dados.**/
+    /**Metodo para conectar o sistema ao banco de dados.**/
     public void Conect(){
         try{
             this.connection = DriverManager.getConnection(DATABASE_URL,
@@ -35,12 +42,43 @@ public abstract class BancoDeDados {
             System.out.println("Não foi possivel conectar ao banco de dados");
         }
     }
-
+    /**Metodo publico que cadastra um cliente no banco de dados.
+     *
+     * @param cliente
+     */
+    public void CadastraCliente(Cliente cliente){
+        String cmd = "INSERT INTO cliente (nome,cpf,rg,rua,bairro,cidade,cep)"+
+                "VALUES ("+cliente.getNome()+","+cliente.getCpf()+
+                ","+cliente.getRg()+","+cliente.getRua()+","+cliente.getBairro()+
+                ","+cliente.getCidade()+","+cliente.getCep()+")";
+        this.ExecuteSQLCmd(cmd);
+    }
+    /**
+     * Metodo publico que cadastra um pruduto no banco de dados.
+     *
+     * @param produto
+     */
+    public void CadastraProduto(Produto produto){
+        String cmd = "INSERT INTO produto () VALUES ()";
+        this.ExecuteSQLCmd(cmd);
+    }
+    /**
+     * Metodo publico que cadastra um servico no banco de dados.
+     *
+     * @param servico
+     **/
+    public void CadastraServico(Servico servico){
+        String cmd = "INSERT INTO servico () VALUES ()";
+        this.ExecuteSQLCmd(cmd);
+    }
+    /**Metodo privado que executa comandos SQL a partir de outros metodos
+     * no banco de dados e retorna o resultado para a variavel resultset.
+     **/
     private void ExecuteSQLCmd(String cmd){
         try{
         this.resultset = this.statement.executeQuery(cmd);
         } catch(SQLException erro){
-            System.out.print("Não foi possivel executa consulta: ");
+            System.out.print("Não foi possivel executa consulta.");
         }
     }
 }
