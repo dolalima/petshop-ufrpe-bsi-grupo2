9ae8105d09da148e2b449405df9b64b54f7e39f7
dolalima@gmail.com <dolalima@gmail.com@1149b242-d79d-a3aa-b50b-b1633aa3294a>
2011-04-25 15:44:45 +0000

    Arquivo teste de consulta de clientes.

---
+++ trunk/PetShop/test/Teste/TesteClienteList.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Teste;
+
+import petshop.classes.BancoDeDados;
+import petshop.classes.Cliente;
+
+/**
+ *
+ * @author dola
+ */
+public class TesteClienteList {
+    private static Cliente[] c;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BancoDeDados.conectar();
+        
+        c = BancoDeDados.consultarCliente("LIMA", "nome");
+        System.out.printf("Resultado da Pesquisa: %d \n",c.length);
+
+
+        for (int i=0;i<c.length;i++){
+            System.out.println(c[i].getNome());
+        }
+        
+
+        // TODO code application logic here
+    }
+
+}
