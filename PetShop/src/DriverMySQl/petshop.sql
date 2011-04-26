47c123fab87c58f4e32c4389e4d21a38e84c103a
dolalima <dolalima@gmail.com>
2011-04-26 00:25:10 +0000

    Atualização do banco de dados

--- PetShop/src/DriverMySQl/petshop.sql
+++ PetShop/src/DriverMySQl/petshop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: Abr 21, 2011 as 10:29 AM
+-- Tempo de Geração: Abr 25, 2011 as 03:12 PM
 -- Versão do Servidor: 5.1.49
 -- Versão do PHP: 5.3.3-1ubuntu9.3
 
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 
 INSERT INTO `cliente` (`id_cliente`, `nome`, `sexo`, `cpf`, `rg`, `rua`, `ncasa`, `bairro`, `cidade`, `complemento`, `uf`, `telefone`, `celular`, `cep`, `info`) VALUES
 (1, 'CARLOS EDUARDO DE SOUZA LIMA', 'Masculino', '876.858.757-57', 7667667, 'JARDIM', 74, 'LIMAMENTO', 'VITORIA', 'Complemento', 'PI', '(76)3673-6376', '(72)7273-6772', '65556-566', 'Informações Adicionais'),
-(2, 'MARIA EDUARDA', 'Feminino', '070.058.184-74', 7153203, 'JARDIM BETANIA', 69, 'LIVRAMENTO', 'RECIFE', 'Complemento', 'PE', '(87)2727-2789', '(99)8387-7363', '55602-380', 'Informações Adicionais');
+(2, 'MARIA EDUARDA DE SOUZA LIMA', 'Feminino', '070.058.184-74', 7153203, 'JARDIM BETANIA', 69, 'LIVRAMENTO', 'RECIFE', 'Complemento', 'PE', '(87)2727-2789', '(99)8387-7363', '55602-380', 'Informações Adicionais');
 
 -- --------------------------------------------------------
 
@@ -84,14 +84,40 @@ CREATE TABLE IF NOT EXISTS `produtos` (
   `codigo` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `qt` int(11) NOT NULL,
-  `preco_custo` float NOT NULL,
-  `preco_venda` float NOT NULL,
+  `preco_custo` double NOT NULL,
+  `preco_venda` double NOT NULL,
+  `info` varchar(200) NOT NULL,
   PRIMARY KEY (`codigo`),
   UNIQUE KEY `id_produto` (`codigo`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`codigo`, `nome`, `qt`, `preco_custo`, `preco_venda`, `info`) VALUES
+(10001, 'RACAO PEDGRIR 1 KG', 5, 5, 10, 'MARCA BOA'),
+(10002, 'RACAO PEDGRIR 2 KG', 7, 3.5, 7.5, 'MARCA BOA'),
+(11001, 'RACAO MAGNUS 10 KG', 15, 10, 14.5, 'RACAO DE MEIA QUALIDADE'),
+(11002, 'RACAO MAGNUS 15 KG', 7, 12.3, 18.99, 'RACAO DE MEIA QUALIDADE');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `servicos`
+--
+
+CREATE TABLE IF NOT EXISTS `servicos` (
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `duracao` int(3) NOT NULL,
+  `preco` double NOT NULL,
+  `info` varchar(200) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `servicos`
 --
 
 
