68ee7ed5dca52b97c0b5964660ffbfa24a0a9a3e
dolalima <dolalima@1149b242-d79d-a3aa-b50b-b1633aa3294a>
2011-05-01 22:33:39 +0000

    Adicionado metodos getAnimalFromCliente , checkCPF e remover na classe anco de dados | Atualização do Bando de Dados

--- trunk/PetShop/src/DriverMySQl/petshop.sql
+++ trunk/PetShop/src/DriverMySQl/petshop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: Mai 01, 2011 as 02:43 PM
+-- Tempo de Geração: Mai 01, 2011 as 07:30 PM
 -- Versão do Servidor: 5.1.49
 -- Versão do PHP: 5.3.3-1ubuntu9.3
 
@@ -61,22 +61,22 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `cidade` varchar(25) NOT NULL,
   `complemento` varchar(100) NOT NULL,
   `uf` varchar(2) NOT NULL,
+  `email` varchar(30) NOT NULL,
   `telefone` varchar(13) NOT NULL,
   `celular` varchar(13) NOT NULL,
   `cep` varchar(9) NOT NULL,
   `info` varchar(200) NOT NULL,
   `ativo` tinyint(1) NOT NULL,
-  PRIMARY KEY (`codigo`),
-  UNIQUE KEY `id_cliente` (`codigo`)
+  PRIMARY KEY (`codigo`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Extraindo dados da tabela `cliente`
 --
 
-INSERT INTO `cliente` (`codigo`, `nome`, `sexo`, `cpf`, `rg`, `rua`, `ncasa`, `bairro`, `cidade`, `complemento`, `uf`, `telefone`, `celular`, `cep`, `info`, `ativo`) VALUES
-(1, 'CARLOS EDUARDO DE SOUZA LIMA', 'M', '876.858.757-57', 7667667, 'JARDIM', 74, 'LIMAMENTO', 'VITORIA', 'Complemento', 'PI', '(76)3673-6376', '(72)7273-6772', '65556-566', 'Informações Adicionais', 0),
-(2, 'MARIA EDUARDA DE SOUZA LIMA', 'F', '070.058.184-74', 7153203, 'JARDIM BETANIA', 69, 'LIVRAMENTO', 'RECIFE', 'Complemento', 'PE', '(87)2727-2789', '(99)8387-7363', '55602-380', 'Informações Adicionais', 0);
+INSERT INTO `cliente` (`codigo`, `nome`, `sexo`, `cpf`, `rg`, `rua`, `ncasa`, `bairro`, `cidade`, `complemento`, `uf`, `email`, `telefone`, `celular`, `cep`, `info`, `ativo`) VALUES
+(1, 'CARLOS EDUARDO DE SOUZA LIMA', 'M', '876.858.757-57', 7667667, 'JARDIM', 74, 'LIMAMENTO', 'VITORIA', 'Complemento', 'PI', '', '(76)3673-6376', '(72)7273-6772', '65556-566', 'Informações Adicionais', 1),
+(2, 'MARIA EDUARDA DE SOUZA LIMA', 'F', '070.058.184-74', 7153203, 'JARDIM BETANIA', 69, 'LIVRAMENTO', 'RECIFE', 'Complemento', 'PE', '', '(87)2727-2789', '(99)8387-7363', '55602-380', 'Informações Adicionais', 1);
 
 -- --------------------------------------------------------
 
@@ -91,6 +91,7 @@ CREATE TABLE IF NOT EXISTS `produtos` (
   `preco_custo` double NOT NULL,
   `preco_venda` double NOT NULL,
   `info` varchar(200) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
   PRIMARY KEY (`codigo`),
   UNIQUE KEY `id_produto` (`codigo`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -99,11 +100,11 @@ CREATE TABLE IF NOT EXISTS `produtos` (
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`codigo`, `nome`, `qt`, `preco_custo`, `preco_venda`, `info`) VALUES
-(10001, 'RACAO PEDGRIR 1 KG', 5, 5, 10, 'MARCA BOA'),
-(10002, 'RACAO PEDGRIR 2 KG', 7, 3.5, 7.5, 'MARCA BOA'),
-(11001, 'RACAO MAGNUS 10 KG', 15, 10, 14.5, 'RACAO DE MEIA QUALIDADE'),
-(11002, 'RACAO MAGNUS 15 KG', 7, 12.3, 18.99, 'RACAO DE MEIA QUALIDADE');
+INSERT INTO `produtos` (`codigo`, `nome`, `qt`, `preco_custo`, `preco_venda`, `info`, `ativo`) VALUES
+(10001, 'RACAO PEDGRIR 1 KG', 5, 5, 10, 'MARCA BOA', 0),
+(10002, 'RACAO PEDGRIR 2 KG', 7, 3.5, 7.5, 'MARCA BOA', 0),
+(11001, 'RACAO MAGNUS 10 KG', 15, 10, 14.5, 'RACAO DE MEIA QUALIDADE', 0),
+(11002, 'RACAO MAGNUS 15 KG', 7, 12.3, 18.99, 'RACAO DE MEIA QUALIDADE', 0);
 
 -- --------------------------------------------------------
 
@@ -135,7 +136,9 @@ CREATE TABLE IF NOT EXISTS `saidaservicos` (
   `codigo` int(11) NOT NULL,
   `venda` int(11) NOT NULL,
   `servico` int(11) NOT NULL,
-  `valor` double NOT NULL
+  `animal` int(11) NOT NULL,
+  `valor` double NOT NULL,
+  PRIMARY KEY (`codigo`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -155,6 +158,7 @@ CREATE TABLE IF NOT EXISTS `servicos` (
   `duracao` int(3) NOT NULL,
   `preco` double NOT NULL,
   `info` varchar(200) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
   PRIMARY KEY (`codigo`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -162,8 +166,10 @@ CREATE TABLE IF NOT EXISTS `servicos` (
 -- Extraindo dados da tabela `servicos`
 --
 
-INSERT INTO `servicos` (`codigo`, `nome`, `duracao`, `preco`, `info`) VALUES
-(5001, 'BANHO POODLE', 30, 20, 'BLABLABLa');
+INSERT INTO `servicos` (`codigo`, `nome`, `duracao`, `preco`, `info`, `ativo`) VALUES
+(5001, 'BANHO POODLE', 30, 20, 'BLABLABLa', 0),
+(5002, 'TOSA POODLE', 60, 30, 'Informações Adicionais', 0),
+(5003, 'BANHO POOODLE', 45, 60, 'Informações Adicionais', 0);
 
 -- --------------------------------------------------------
 
@@ -198,7 +204,8 @@ CREATE TABLE IF NOT EXISTS `vendas` (
   `id_vendas` int(11) NOT NULL,
   `cliente` int(11) NOT NULL,
   `valor` double NOT NULL,
-  `pagamento` int(11) NOT NULL
+  `pagamento` int(11) NOT NULL,
+  PRIMARY KEY (`id_vendas`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
